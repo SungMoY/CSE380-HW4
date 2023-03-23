5f98a0bea3c20139fd6e5712e741f0b515c60f30
SungMoY <90225118+SungMoY@users.noreply.github.com>
2023-03-23 12:37:18 -0400

    wip

--- src/hw4/Pathfinding/AstarStrategy.ts
+++ src/hw4/Pathfinding/AstarStrategy.ts
@@ -1,11 +1,11 @@
+import List from "../../Wolfie2D/DataTypes/Collections/List";
 import Stack from "../../Wolfie2D/DataTypes/Collections/Stack";
 import Vec2 from "../../Wolfie2D/DataTypes/Vec2";
 import NavigationPath from "../../Wolfie2D/Pathfinding/NavigationPath";
 import NavPathStrat from "../../Wolfie2D/Pathfinding/Strategies/NavigationStrategy";
 import GraphUtils from "../../Wolfie2D/Utils/GraphUtils";
-import AstarNode from "./AstarNode";
 
-// TODO Construct a NavigationPath object using A*
+// TODO letruct a NavigationPath object using A*
 /**
  * All nodes have G: min distance from origin node (A) to this node, and H: estimate distance to goal node
  * The key is the minimize G + H to get the shortest, best path
@@ -36,33 +36,100 @@ export default class AstarStrategy extends NavPathStrat {
      * @see NavPathStrat.buildPath()
      */
     public buildPath(to: Vec2, from: Vec2): NavigationPath {
-        let openList = [];
-        let closedList = [];
-        // Get the closest nodes in the graph to our to and from positions
-        let start = this.mesh.graph.snap(from);
-		let end = this.mesh.graph.snap(to);
 
-        openList.push()
-        this.calcHeuristic(start, end);
+        let start = this.mesh.graph.snap(from)
+        let end = this.mesh.graph.snap(to)
+        let openList = [start]
+        let closedList = []
+        let data = {}
+        let parent = {}
 
+        data[start] = { g: 0, h: this.heuristic(start, end), f: this.heuristic(start, end) };
+        parent[start] = -1;
 
-        return new NavigationPath(new Stack<Vec2>());
+        while (openList.length > 0) {
+            // Sort openList by f score
+            openList.sort((a, b) => data[a].f - data[b].f);
+            let current = openList.shift();
+            closedList.push(current);
+        
+            if (current === end) {
+                // Reletruct path
+                console.log("PATH FOUND")
+                return new NavigationPath(this.endAlgo(parent, end));
+            }
+        
+            let neighbors = this.getNeighbors(current);
+            for (let neighbor of neighbors) {
+                if (closedList.includes(neighbor)) continue;
+                //also check if neighbor has three edges. if it has less, skip it
+                let edges = this.mesh.graph.getEdges(neighbor)
+                let numEdges = 0
+                while (edges.next !== null) {
+                    numEdges++
+                    edges = edges.next
+                }
+                if (numEdges < 3) continue;
+            
+                let newPathToNeighbor = data[current].g + this.getWeight(current, neighbor);
+                if (newPathToNeighbor < data[neighbor]?.g || !openList.includes(neighbor)) {
+                    data[neighbor] = { g: newPathToNeighbor, h: this.heuristic(neighbor, end), f: newPathToNeighbor + this.heuristic(neighbor, end) };
+                    parent[neighbor] = current;
+                    if (!openList.includes(neighbor)) openList.push(neighbor);
+                }
+            }
+        }
+        
+        // No path found
+        console.log("NO PATH FOUND")
+        return new NavigationPath(new Stack());    
     }
 
-    // start and end are Nodes in this.mesh.graph that closely correlate with positions
-    private calcHeuristic(start: number, end: number): number {
-        // calculate the estimated distance from start to end
-        // need to take into account obstacles and walls
-
-        // the graph has edges that are distances between nodes
-        let edges = this.mesh.graph.getEdges(start);
+    // returns an array of the neighbors of a node
+    private getNeighbors(node) {
+        let neighbors = []
+        let edges = this.mesh.graph.getEdges(node)
         while (edges.next !== null) {
-            console.log(this.mesh.graph.getNodePosition(start)+
-            " is "+ edges.weight+
-            " to "+this.mesh.graph.getNodePosition(edges.y));
-            edges = edges.next;
+            neighbors.push(edges.y)
+            edges = edges.next
         }
+        return neighbors
+    }
 
-        return 0;
+    // returns the weight of an edge between two nodes
+    private getWeight(node1, node2) {
+        let edges = this.mesh.graph.getEdges(node1)
+        while (edges.next !== null) {
+            if (edges.y === node2) {
+                // square then square root to get positive number
+                return Math.sqrt(edges.weight*edges.weight)
+            }
+            edges = edges.next
+        }
+        return 0
+    }
+
+    // returns the heuristic value of a node
+    private heuristic(node, end) {
+        let nodePos = this.mesh.graph.getNodePosition(node)
+        let endPos = this.mesh.graph.getNodePosition(end)
+        let distX = Math.abs(nodePos.x - endPos.x)
+        let distY = Math.abs(nodePos.y - endPos.y)
+        if (distX > distY) {
+            return 14*distY + 10*(distX-distY)
+        }
+        return 14*distX + 10*(distY-distX)
+    }
+
+    // creates the path stack for this function to return using parents
+    private endAlgo(parent, end): Stack<Vec2> {
+        console.log("CONSTRUCTING PATH")
+        let path = new Stack<Vec2>(this.mesh.graph.numVertices)
+        let current = end
+        while (current !== -1) {
+            path.push(this.mesh.graph.getNodePosition(current))
+            current = parent[current]
+        }
+        return path
     }
 }
